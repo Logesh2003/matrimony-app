e5b59c543c761b8a89ad9c9c1b09d506335a4c47
logesh <gm.logeshhari2003@gmail.com>
2026-01-07 17:18:37 +0530

    Day 2: MongoDB localhost connection setup

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,23 @@
 const express = require("express");
 const cors = require("cors");
+const connectDB = require("./config/db");
 require("dotenv").config();
 
 const app = express();
 
-// Middlewares
+// Connect DB
+connectDB();
+
+// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Test route
+// Routes
 app.get("/", (req, res) => {
-    res.send("Matrimony API is running 🚀");
+    res.send("Matrimony API running 🚀");
 });
 
-// Server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    console.log(`Server started on port ${PORT}`);
 });
